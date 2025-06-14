dc64e2c85acd8f164491b54425a34a52ef39aa57
Haseeb-Ullah-01 <93939012+Haseeb-Ullah-01@users.noreply.github.com>
2025-06-14 21:59:30 +0100

    Add Render domain to ALLOWED_HOSTS

--- hjbrand/settings.py
+++ hjbrand/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-!2mfzo+=-)&#r7zb5ydj!!covn(*)%+h^5v(!=4@2w3t(h(s2$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
